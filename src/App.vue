98e0c42abebec55528926d18384addefcf1b9d1e
Jornay <lucasdeafg1@gmail.com>
2021-11-24 01:30:07 -0300

    fix.automating the project

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@
             {{ link }}
           </v-btn>
           <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
-            {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+            {{ new Date().getFullYear() }} — <strong>Food List</strong>
           </v-col>
         </v-row>
       </v-footer>
