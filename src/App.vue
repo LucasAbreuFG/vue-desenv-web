e301442d31d3c3f280090f830f36807511f520a6
Jornay <lucasdeafg1@gmail.com>
2021-11-23 21:53:25 -0300

    fix.page-app

--- src/App.vue
+++ src/App.vue
@@ -1,105 +1,112 @@
 <template>
-  <v-app>
-    <template>
+  <v-app >
+    <v-app-bar app style="margin-top: 0px;">
       <div>
-        <v-system-bar app dark color="primary">
-          <v-spacer></v-spacer>
-          <v-icon>mdi-wifi-strength-4</v-icon>
-          <v-icon>mdi-signal-cellular-outline</v-icon>
-          <v-icon>mdi-battery</v-icon>
-          <span>12:30</span>
-        </v-system-bar>
-      </div>
-    </template>
+          <v-system-bar app dark color="primary">
+            <v-spacer></v-spacer>
+            <v-icon>mdi-wifi-strength-4</v-icon>
+            <v-icon>mdi-signal-cellular-outline</v-icon>
+            <v-icon>mdi-battery</v-icon>
+            <span>12:30</span>
+          </v-system-bar>
+        </div>
 
-    <v-main>
+        <v-container fluid>
+          <v-card
+          class="overflow-hidden"
+          style="position: absolute; top: 2.5%; right: 0; width: 100%;"
+        >
+          <v-app-bar
+            absolute
+            color="#fcb69f"
+            dark
+            shrink-on-scroll
+            src="https://picsum.photos/1920/1080?random"
+            scroll-target="#scrolling-techniques-2"
+            style="padding-top: 0.8rem; "
+          >
+            <template v-slot:img="{ props }">
+              <v-img
+                v-bind="props"
+                gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+              ></v-img>
+            </template>
+
+            <v-app-bar-title style="padding-bottom: 2vh;">Space Page</v-app-bar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </v-app-bar>
+          <v-sheet
+            id="scrolling-techniques-2"
+            class="overflow-y-auto"
+            max-height="600"
+          >
+            <v-container style="height: 128px"></v-container>
+          </v-sheet>
+        </v-card>  
+      </v-container>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main style=" padding: 0px 0px 0px;">
       <router-view />
+      <!-- Provides the application the proper gutter -->
+      
     </v-main>
-    <v-card
-      class="overflow-hidden"
-      style="position: absolute; top: 2.5%; right: 0; width: 100%"
-    >
-      <v-app-bar
-        absolute
-        color="#fcb69f"
-        dark
-        shrink-on-scroll
-        src="https://picsum.photos/1920/1080?random"
-        scroll-target="#scrolling-techniques-2"
-      >
-        <template v-slot:img="{ props }">
-          <v-img
-            v-bind="props"
-            gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
-          ></v-img>
-        </template>
 
-        <v-app-bar-title>Space Page</v-app-bar-title>
+    <v-footer app>
+      <v-footer app color="primary lighten-1" padless>
+        <v-row justify="center" no-gutters>
+          <v-btn
+            v-for="link in links"
+            :key="link"
+            color="white"
+            text
+            rounded
+            class="my-2"
+          >
+            {{ link }}
+          </v-btn>
+          <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
+            {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+          </v-col>
+        </v-row>
+      </v-footer>
 
-        <v-spacer></v-spacer>
+      <v-bottom-navigation :value="value"  app color="teal" grow>
+        <v-btn>
+          <span>Recents</span>
 
-        <v-btn icon>
-          <v-icon>mdi-magnify</v-icon>
+          <v-icon>mdi-history</v-icon>
         </v-btn>
 
-        <v-btn icon>
+        <v-btn>
+          <span>Favorites</span>
+
           <v-icon>mdi-heart</v-icon>
         </v-btn>
 
-        <v-btn icon>
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </v-app-bar>
-      <v-sheet
-        id="scrolling-techniques-2"
-        class="overflow-y-auto"
-        max-height="600"
-      >
-        <v-container style="height: 128px"></v-container>
-      </v-sheet>
-    </v-card>
+        <v-btn>
+          <span>Nearby</span>
 
-    <v-footer app color="primary lighten-1" padless>
-      <v-row justify="center" no-gutters>
-        <v-btn
-          v-for="link in links"
-          :key="link"
-          color="white"
-          text
-          rounded
-          class="my-2"
-        >
-          {{ link }}
+          <v-icon>mdi-map-marker</v-icon>
         </v-btn>
-        <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
-          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-        </v-col>
-      </v-row>
+      </v-bottom-navigation>
     </v-footer>
-    <v-bottom-navigation :value="value"  app color="teal" grow>
-      <v-btn>
-        <span>Recents</span>
-
-        <v-icon>mdi-history</v-icon>
-      </v-btn>
-
-      <v-btn>
-        <span>Favorites</span>
-
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn>
-        <span>Nearby</span>
-
-        <v-icon>mdi-map-marker</v-icon>
-      </v-btn>
-    </v-bottom-navigation>
   </v-app>
 </template>
-
-
-
 
 <script>
 export default {
